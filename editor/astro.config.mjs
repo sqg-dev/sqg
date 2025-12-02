7544ed05c1ad61f8e4b04dbc9c7896f1967a5964
Uwe Maurer <uwe@apptornado.com>
2025-12-02 19:31:30 +0100

    use node server instead of static content use astro action to generate code

--- editor/astro.config.mjs
+++ editor/astro.config.mjs
@@ -2,8 +2,13 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
   integrations: [svelte(), tailwind()],
 });
